0788c9b30fb6cd7366c2f035eadd397f630649ff
IT23621374 <IT23621374@my.sliit.lk>
2026-04-15 06:55:50 +0530

    Add files via upload

--- backend/src/main/java/com/smartcampus/SmartCampusApplication.java
+++ backend/src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -1,18 +1,14 @@
 package com.smartcampus;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class SmartCampusApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-        
         SpringApplication.run(SmartCampusApplication.class, args);
     }
 }
-
